39b4dd86441d9a7b0a1b28bc43e16216329c46e1
tedfmyers <tedfmyers@gmail.com>
2017-09-16 17:31:33 -0400

    Application can send and receive
    Sends packets at 2Hz interval

--- application/application.ino
+++ application/application.ino
@@ -9,6 +9,11 @@
   #include <avr/power.h>
 #endif
 
+// Function prototypes
+void init_timer1(void);
+
+volatile uint8_t counter;
+
 // neopixel
 #define PIN            6
 #define NUMPIXELS      3
@@ -25,18 +30,18 @@ Adafruit_SSD1306 display(OLED_RESET);
 volatile int nCounter = 1;
 
 RF24 radio(7, 8); // CNS, CE
-
 const byte address[6] = "00001";
 
 void setup() {
-  
 #if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
 
-  pixels.begin(); // This initializes the NeoPixel library.
+  init_timer1();
 
-  Serial.begin(9600);
+  pixels.begin(); // Initialize NeoPixel Library
+
+//  Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_HIGH);
@@ -53,7 +58,7 @@ void setup() {
   
   //Initialize text
   display.clearDisplay();
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
 
@@ -61,37 +66,55 @@ void setup() {
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.println("Testing");
-  display.println("Display Test Program");
+  display.println("Display Test - waiting for input");
   display.setTextSize(1);
   display.setTextColor(BLACK, WHITE);
   display.println("Insert message here!");
   display.display();
   
-  delay(2000);
+  delay(1000);
   showSplash = 0;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+//occurs w/frequency of 2Hz
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.stopListening();
+  
+  counter++;
+  const char text[] = "Hello World";
+  char counter_string[16] = {0};
+  sprintf(counter_string, "Hello World: %d", counter);
+  radio.write(&counter_string, sizeof(counter_string));
+
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.startListening();
 }
 
 void loop() {
 
+  //sei();//allow interrupts (starts sending packets)
+
+  // For now, just test colors
   for(int i=0;i<NUMPIXELS;i++){
     //RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(255-nCounter,nCounter,255-nCounter));
 
     pixels.show(); // This sends the updated pixel color to the hardware.
-   //delay(delayval); // Delay for a period of time (in milliseconds).
   }
   
   if (radio.available()) {
-    char text[32] = "";
+    char text[128] = "";
     radio.read(&text, sizeof(text));
-    Serial.println(text);
     digitalWrite(2, HIGH); // flash LED
 
     // display stuff
     display.clearDisplay();
     display.setCursor(0,0);
-    display.setTextSize(2);
+    display.setTextSize(1);
     display.setTextColor(WHITE);
     display.print(text);
     display.display();
@@ -105,16 +128,23 @@ void loop() {
   digitalWrite(2, LOW); // turn off LED
 }
 
-//// Displays nSides, nCounter, and nResult
-//void displayScreen()
-//{
-//  display.clearDisplay();
-//  display.setCursor(0,0);
-//  display.setTextSize(2);
-//  display.setTextColor(WHITE);
-//  
-//  display.print(nCounter);
-//  
-//  display.display();
-//  
-//}
+void init_timer1()
+{
+  cli();//stop interrupts
+
+  //set timer1 interrupt at 2Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 7811;// = (16*10^6) / (2*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+  sei();//allow interrupts
+}
+
